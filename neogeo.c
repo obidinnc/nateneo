f479f88b8efaa60f41091d4bbc958e9eb95857e1
obidinnc <59855013+obidinnc@users.noreply.github.com>
2020-02-03 01:10:40 -0500

    Update neogeo.c

--- neogeo.c
+++ neogeo.c
@@ -2,5 +2,5 @@ Main()
 {
  printf("Hello there!\n");
  printf("I like the NEO GEO.\n");
- printf(My favorite game is Mark of the Wolves.\n");
+ printf("My favorite game is Mark of the Wolves.\n");
 }
